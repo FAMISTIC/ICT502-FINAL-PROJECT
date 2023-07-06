1b370ecc3278ab94baa0abb4e38fb496734702bd
FAMISTIC <2021829874@student.uitm.edu.my>
2023-07-06 11:37:58 +0800

    add vehicle registration

--- register-customer-vehicle.php
+++ register-customer-vehicle.php
@@ -6,16 +6,30 @@ if(isset($_POST['save']))
 	$phone = $_POST['phone'];
 	$password = $_POST['password'];
 	$email = $_POST['email'];
+
+	$model = $_POST['model'];
+	$colour = $_POST['colour'];
+	$plate = $_POST['plate'];
+
 	$query = oci_parse($connection, "INSERT INTO customer(customer_name,phone,password,email) 
 	values ('$customer_name','$phone','$password','$email')");
+	$query2 = oci_parse($connection, "INSERT INTO vehicle(model,colour,plate) 
+	values ('$model','$colour','$plate')");
+
 	$result = oci_execute($query);
-	if ($result) {
-				echo "Data added Successfully !";
+	$result2 = oci_execute($query2);
+
+
+	if ($result && $result2) {
+				echo "User Data added Successfully !";
 				exit();
 	}
 	else{
 		echo "Error !";
 				exit();
 	}
+	
+
+	
 }
 ?>
