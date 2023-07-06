8a5a4c1b25345418033b4f64658b039f534b9342
FAMISTIC <2021829874@student.uitm.edu.my>
2023-07-06 12:32:11 +0800

    validation

--- register-customer-vehicle.php
+++ register-customer-vehicle.php
@@ -1,35 +1,69 @@
 <?php
 include_once 'connection.php';
-if(isset($_POST['save']))
-{	 
-	$customer_name = $_POST['customer_name'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['customer_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 	$phone = $_POST['phone'];
-	$password = $_POST['password'];
-	$email = $_POST['email'];
 
 	$model = $_POST['model'];
 	$colour = $_POST['colour'];
 	$plate = $_POST['plate'];
 
-	$query = oci_parse($connection, "INSERT INTO customer(customer_name,phone,password,email) 
-	values ('$customer_name','$phone','$password','$email')");
-	$query2 = oci_parse($connection, "INSERT INTO vehicle(model,colour,plate) 
-	values ('$model','$colour','$plate')");
 
-	$result = oci_execute($query);
-	$result2 = oci_execute($query2);
+    // Check if the username or email already exists in the database
+    $query = "SELECT * FROM customer WHERE customer_name = :customer_name OR email = :email OR phone = :phone OR password = :password";
+    $stmt = oci_parse($connection, $query);
+    oci_bind_by_name($stmt, ':customer_name', $customer_name);
+    oci_bind_by_name($stmt, ':email', $email);
+	oci_bind_by_name($stmt, ':password', $password);
+    oci_bind_by_name($stmt, ':phone', $phone);
+    oci_execute($stmt);
+
+	$query2 = "SELECT * FROM vehicle WHERE plate = :plate";
+	$stmt2 = oci_parse($connection, $query2);
 
 
-	if ($result && $result2) {
-				echo "User Data added Successfully !";
-				exit();
-	}
-	else{
-		echo "Error !";
-				exit();
-	}
-	
 
-	
+
+    if (oci_fetch($stmt) || oci_fetch($stmt2)) {
+        // Username or email already exists
+        echo 'Username or email already taken.';
+    } else {
+
+		
+        // Insert the new user into the database
+		if(isset($_POST['save']))
+		{	 
+		
+		
+			$query = oci_parse($connection, "INSERT INTO customer(customer_name,phone,password,email) 
+			values ('$customer_name','$phone','$password','$email')");
+			$query2 = oci_parse($connection, "INSERT INTO vehicle(model,colour,plate) 
+			values ('$model','$colour','$plate')");
+		
+			$result = oci_execute($query);
+			$result2 = oci_execute($query2);
+		
+		
+			if ($result && $result2) {
+						echo "User Data added Successfully !";
+						exit();
+			}
+			else{
+				echo "Error !";
+						exit();
+			}
+			
+		
+			
+		}
+    }
+
+    // Close the Oracle connection
+    oci_close($connection);
 }
+
+
 ?>
